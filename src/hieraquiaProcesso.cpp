45bfd16514f0b5cb739a5a25c4cf30ace792fb77
Samuel Lucas de Moura Ferino <samuellucas97@A303-02L.imd.local>
2019-03-01 10:41:01 -0300

    adicionando arquivo

---
+++ src/hieraquiaProcesso.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <sys/types.h> /// pid_t
+#include <unistd.h> /// fork()
+#include <dirent.h> /// readdir()
+
+#include <thread>
+using std::thread;
+
+int main(){
+
+
+	DIR *diretorio = nullptr;
+	struct dirent *ent = nullptr;
+	int numeroTotalProcessos = 0;
+	thread threadBackground;
+
+	diretorio = opendir("/proc");
+	
+	if( !diretorio)		/// Verificando se a abertura foi feita corretamente
+		return -1;
+
+	while ( (ent = readdir(diretorio) )!= nullptr ){ /// Fazendo leitura de processos
+		cout << ent->d_name << endl;
+		++numeroTotalProcessos;
+	}
+
+	cout << "Total de processos:" << numeroTotalProcessos << endl;
+	return 0;
+
+}
