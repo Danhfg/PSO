f0b85518b7d49b69715c74da9044ff5fc3cad930
Samuel Lucas <samuel1797@gmail.com>
2019-03-03 02:36:55 -0300

    Update forkBomb.cpp

--- src/forkBomb.cpp
+++ src/forkBomb.cpp
@@ -2,29 +2,26 @@
 using std::cout;
 using std::endl;
 
+#include <thread>
+using std::thread;
+
 #include <unistd.h>  		/// fork()		
 #include <sys/time.h> 		/// pid_t
 #include <sys/resource.h> 	/// rlimit
 
-
 void forkBomb(){
-
 	while(true)	
 		fork();
-
 }
 
 int main(){
 
+	thread thread1(forkBomb);
+	
 	struct rlimit limiteProcessos;
 	getrlimit(RLIMIT_NPROC, &limiteProcessos);
 	
-	cout << limiteProcessos.rlim_cur << endl;	
-	fork();
-	getrlimit(RLIMIT_NPROC, &limiteProcessos);
-	
-	cout << limiteProcessos.rlim_cur << endl;	
-	
+	thread1.join();
 
 	return 0;
 }
