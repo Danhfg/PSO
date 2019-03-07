0c9ca8c16b4a98fc5a9c6d7b7115dba746e6dd61
Samuel Lucas <samuellucas97@projetos.imd.ufrn.br>
2019-03-07 03:12:02 -0300

    atualizando repositório

--- src/questao1/forkBomba.cpp
+++ src/questao1/forkBomba.cpp
@@ -8,27 +8,42 @@
 #include <iostream>
 using std::cout;
 using std::endl;
+using std::cerr;
 
 #include <thread>
 using std::thread;
 
+#include <exception>
+using std::exception;
+
+/// BIBLIOTECAS UNIX/LINUX
 #include <unistd.h>  		/// fork()		
 #include <sys/time.h> 		/// pid_t
 #include <sys/resource.h> 	/// rlimit
 
 void forkBomb(){
-	while(true)	
-		fork();
+	
+	while(true){
+		try{	
+			fork();
+		}
+		catch( exception &e){
+			cerr << "Erro na criação de processo..." << endl;
+		}
+	}			
+
 }
 
 int main(){
 
-	thread thread1(forkBomb);
-	
+	thread thread1(forkBomb); /// Criando e iniciando a thread1 que contém o fork bomba
 	struct rlimit limiteProcessos;
+
 	getrlimit(RLIMIT_NPROC, &limiteProcessos);
-	
-	thread1.join();
+	limiteProcessos.rlim_cur = 3;
+	setrlimit(RLIMIT_NPROC, &limiteProcessos);	/// Modificando a quantidade de processos filhos para 3
+
+	thread1.join(); /// Sincronizando esta thread com a thread Main
 
 	return 0;
 }
