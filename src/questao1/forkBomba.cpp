6a4700f1e126048c5862b5f7b5c17d756bad2b8a
Samuel Lucas de Moura Ferino <samuellucas97@A303-06L.imd.local>
2019-03-08 10:34:59 -0300

    atualizando repositório

--- src/questao1/forkBomba.cpp
+++ src/questao1/forkBomba.cpp
@@ -22,27 +22,28 @@ using std::exception;
 #include <sys/resource.h> 	/// rlimit
 
 void forkBomb(){
-	
-	while(true){
-		try{	
-			fork();
-		}
-		catch( exception &e){
-			cerr << "Erro na criação de processo..." << endl;
-		}
-	}			
+	cout << "OI" << endl;
+		
+	//while(true){
+	//	try{	
+	//		fork();
+	//	}
+	//	catch( exception &e){
+	//		cerr << "Erro na criação de processo..." << endl;
+	//	}
+	//}			
 
 }
 
 int main(){
 
-	thread thread1(forkBomb); /// Criando e iniciando a thread1 que contém o fork bomba
 	struct rlimit limiteProcessos;
 
 	getrlimit(RLIMIT_NPROC, &limiteProcessos);
 	limiteProcessos.rlim_cur = 3;
 	setrlimit(RLIMIT_NPROC, &limiteProcessos);	/// Modificando a quantidade de processos filhos para 3
 
+	thread thread1(forkBomb); /// Criando e iniciando a thread1 que contém o fork bomba
 	thread1.join(); /// Sincronizando esta thread com a thread Main
 
 	return 0;
