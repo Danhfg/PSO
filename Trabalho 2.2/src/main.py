e64ec9b2848c6c8a55c353a0a8880ae0a26ea0e8
Samuel Lucas <samuel1797@gmail.com>
2019-05-20 06:54:43 -0300

    Update main.py

--- Trabalho 2.2/src/main.py
+++ Trabalho 2.2/src/main.py
@@ -1,17 +1,30 @@
+import threading 
+
 from os import system
+from re import split
 
-cmdMemInfo = "grep 'MemTotal\|MemFree\|Cached\|SwapTotal\|SwapFree' /proc/meminfo > memInfo.dat" 
-
-system(cmdMemInfo)
+def tratamentoArquivo( conteudoArquivo ):
+  listaInformacoes = []
+  for linha in conteudoArquivo:
+    c = split('kB|:', linha)
+    listaInformacoes.append(c[1].strip())
+  return listaInformacoes
+    
+def atualizandoArquivo():
+  while True:
+    system("grep 'MemTotal\|MemFree\|Cached\|SwapTotal\|SwapFree' /proc/meminfo > memInfo.dat")
 
 arqMemInfo = open("memInfo.dat", 'r')
 conteudoArqMemInfo = arqMemInfo.readlines()
 
-memInfo = [{'MemTotal': "0"}, {'MemFree': "0"}, {'Cached': "0"}, {'SwapTotal': "0"}, {'SwapFree':"0"}]
+comando = "ps -eo pid,min_flt,maj_flt --no-header | head --lines=-2";
 
 
-memInfo[1]= conteudoArqMemInfo[1].strip()
-memInfo[2] = conteudoArqMemInfo[2].strip()
-memInfo[3] = conteudoArqMemInfo[3].strip()
+print( tratamentoArquivo( conteudoArqMemInfo))
 
-print( memInfo )  
+#memInfo = [{'MemTotal': "0"}, {'MemFree': "0"}, {'Cached': "0"}, {'SwapTotal': "0"}, {'SwapFree':"0"}]
+
+
+t = threading.Thread(target=atualizandoArquivo)
+t.start()
+
