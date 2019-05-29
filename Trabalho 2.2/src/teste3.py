9ea19a608bbef581e52344d38aaa5e74d1c4bdb4
Samuel Lucas <samuel1797@gmail.com>
2019-05-29 00:09:55 -0300

    Update teste3.py

--- Trabalho 2.2/src/teste3.py
+++ Trabalho 2.2/src/teste3.py
@@ -15,6 +15,18 @@ def make_autopct(values):
     return my_autopct
 
 def tratamentoArquivo( conteudoArquivo ):
+    """
+    Realiza o tratamento da string que possui o conteúdo do arquivo
+
+    lista =  tratamentoArquivo(conteudoArquivo)
+
+    INPUT:
+      * conteudoArquivo: Conteudo do arquivo
+
+    return: Lista de informações
+    
+    Maio 2019
+    """
     listaInformacoes = []
     for linha in conteudoArquivo:
         c = split('kB|:', linha)
@@ -24,39 +36,26 @@ def tratamentoArquivo( conteudoArquivo ):
 the_grid = GridSpec(2, 3)
 
 winOpen = True
-
 while winOpen:
     try:
         arqMemInfo = open("memInfo.dat", 'r')
-
         conteudoArqMemInfo = arqMemInfo.readlines()
 
         lista =  tratamentoArquivo(conteudoArqMemInfo)
-
         labels1 = 'Memória Usada', 'Memória Livre'
-
         labels2 = 'Cached', 'SwapCached'
-
         labels3 = 'Swap Total', 'Swap Livre'
 
         fracs1 = [lista[0]-lista[1], lista[1]]
-
         fracs2 = lista[2:4]
-
         fracs3 = [lista[4]-lista[5], lista[5]]
 
         plt.subplot(the_grid[0, 0], aspect=1, title="MONITORANDO RAM")
-
         plt.pie(fracs1, labels=labels1, autopct=make_autopct(fracs1), shadow=True)
-
         plt.subplot(the_grid[0, 2], aspect=1, title="MONITORANDO CACHE")
-
         plt.pie(fracs2, labels=labels2, autopct=make_autopct(fracs2),shadow=True)
-
         plt.subplot(the_grid[1, 1], aspect=1, title="MONITORANDO SWAP")
-
         plt.pie(fracs2, labels=labels3, autopct=make_autopct(fracs2),shadow=True)
-
         plt.draw()
 
         plt.pause(0.5)
